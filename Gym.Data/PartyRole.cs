4aea0253d08c2ed4d580da28285dffb21ab5bdde
Kristi Pihlak <pihlak@taltech.ee>
2026-05-16 19:15:09 +0300

    lisasin PartyRole klassi)

---
+++ Gym.Data/PartyRole.cs
@@ -0,0 +1,17 @@
+﻿using Gym.Data.Common;
+
+namespace Gym.Data;
+
+// Arlowi PartyRole arhetüüp: "roll mida isik mängib", GymMember ja Trainer pärivad sellest
+// Evans DDD: IsActive() on äriloogika meetod, et kuulub KLASSI mitte controllerisse
+public abstract class PartyRole : BaseEntity
+{
+    public Guid PartyId { get; set; }
+    public virtual Person Party { get; set; }
+    public DateTime? RoleStartDate { get; set; } = DateTime.UtcNow;
+    public DateTime? RoleEndDate { get; set; }
+
+    public bool IsActive()
+        => RoleStartDate <= DateTime.UtcNow
+        && (RoleEndDate is null || RoleEndDate >= DateTime.UtcNow);
+}
