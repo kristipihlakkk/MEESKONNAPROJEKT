34b18148d1d04fc3bc27ebe0203e25d29b259404
Martin Persidski <mapers@taltech.ee>
2026-05-18 23:17:16 +0300

    Person ja PersonRepo testid

--- GymSystem.Client/Program.cs
+++ GymSystem.Client/Program.cs
@@ -1,5 +1,9 @@
+using Gym.Infra;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 await builder.Build().RunAsync();
+
+builder.Services.AddScoped<IPersonsRepo, PersonsRepo>();
+
---
+++ Tests/Data/PersonTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Gym.Data;
+
+namespace Gym.Tests.Data
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void FullName_ShouldReturnFirstNameAndLastName()
+        {
+            // Arrange
+            var person = new Person { FirstName = "John", LastName = "Doe" };
+
+            // Act
+            var fullName = person.FullName;
+
+            // Assert
+            Assert.AreEqual("John Doe", fullName);
+        }
+
+        [TestMethod]
+        public void FullName_WithEmptyFirstName_ShouldReturnOnlyLastName()
+        {
+            // Arrange
+            var person = new Person { FirstName = "", LastName = "Doe" };
+
+            // Act
+            var fullName = person.FullName;
+
+            // Assert
+            Assert.AreEqual(" Doe", fullName);
+        }
+
+        [TestMethod]
+        public void FullName_WithEmptyLastName_ShouldReturnOnlyFirstName()
+        {
+            // Arrange
+            var person = new Person { FirstName = "John", LastName = "" };
+
+            // Act
+            var fullName = person.FullName;
+
+            // Assert
+            Assert.AreEqual("John ", fullName);
+        }
+
+        [TestMethod]
+        public void FirstName_ShouldHaveRequiredAttribute()
+        {
+            // Arrange
+            var property = typeof(Person).GetProperty("FirstName");
+
+            // Act
+            var attributes = property.GetCustomAttributes(typeof(RequiredAttribute), true);
+
+            // Assert
+            Assert.IsTrue(attributes.Length > 0, "FirstName should have [Required] attribute");
+        }
+
+        [TestMethod]
+        public void LastName_ShouldHaveRequiredAttribute()
+        {
+            // Arrange
+            var property = typeof(Person).GetProperty("LastName");
+
+            // Act
+            var attributes = property.GetCustomAttributes(typeof(RequiredAttribute), true);
+
+            // Assert
+            Assert.IsTrue(attributes.Length > 0, "LastName should have [Required] attribute");
+        }
+
+        [TestMethod]
+        public void Email_ShouldHaveRequiredAndEmailAddressAttributes()
+        {
+            // Arrange
+            var property = typeof(Person).GetProperty("Email");
+
+            // Act
+            var requiredAttributes = property.GetCustomAttributes(typeof(RequiredAttribute), true);
+            var emailAttributes = property.GetCustomAttributes(typeof(EmailAddressAttribute), true);
+
+            // Assert
+            Assert.IsTrue(requiredAttributes.Length > 0, "Email should have [Required] attribute");
+            Assert.IsTrue(emailAttributes.Length > 0, "Email should have [EmailAddress] attribute");
+        }
+    }
+}
