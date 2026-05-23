6708b3ff04ef182e551040231d40a4c66c17d682
Kristi Pihlak <pihlak@taltech.ee>
2026-05-23 19:51:23 +0300

    Lisasin PartyRole

--- Gym.Data/Person.cs
+++ Gym.Data/Person.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Gym.Data.Common;
+using System.Net;
 
-namespace Gym.Data
+namespace Gym.Data;
+// Silverstoni järgi peavad Address ja ContactMethod olema ERALDI tabelites
+public class Person : Party
 {
-    internal class Person
-    {
-    }
+    public string PersonalCode { get; set; } = string.Empty;
+    public DateTime? DateOfBirth { get; set; }
+    public string Gender { get; set; } = string.Empty;
+
+    // Silverston: aadressid eraldi tabelis
+    public virtual ICollection<Address> Addresses { get; set; }
+    = new List<Address>();
+
+    // Silverston: kontaktid eraldi tabelis
+   public virtual ICollection<ContactMethod> ContactMethods { get; set; }
+   = new List<ContactMethod>();
 }
