4aa392bedbfca51663c5dcb93b2e7ba7301e86eb
Nora Männilaan <norama@taltech.ee>
2026-05-23 20:13:15 +0300

    Lisasin Persons page aadressi ja contact methodid

--- Code/Infra/Repos/Repos.cs
+++ Code/Infra/Repos/Repos.cs
@@ -6,8 +6,13 @@ using Microsoft.EntityFrameworkCore;
 
 
 namespace Gym.Infra;
-
 public sealed class PersonsRepo : BaseRepo<Person>, IPersonsRepo
 {
     public PersonsRepo(AppDbContext context) : base(context) { }
+
+    public override async Task<Person?> GetAsync(Guid id)
+        => await _context.Persons
+            .Include(p => p.Addresses)
+            .Include(p => p.ContactMethods)
+            .FirstOrDefaultAsync(p => p.Id == id);
 }
